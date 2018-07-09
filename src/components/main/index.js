6afe8fe102a12caaa909d515af2407b5866e2f4d
Alex Caudill <alex.caudill@protonmail.com>
2018-07-09 00:11:03 -0500

    remove broken router example

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -81,7 +81,6 @@ export default () => (
       <li><Link to="/page/about">About</Link></li>
       <li><Link to="/page/contact">Contact</Link></li>
     </ul>
-    Change routes anywhere &mdash; <button onClick={changeRoute}>Like here (About)</button>
     <hr />
     <Switch>
       <Route exact path="/" component={Home} />
